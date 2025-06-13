99ffbc7f4e9f49f511efe62aab71ecfc3f83a2ba
¨Leonardo¨ <ltoralhernandez@gmail.com>
2025-06-13 15:38:11 -0600

    first commit

---
+++ src/main/java/com/Team3/LibraryProject/Controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.Team3.LibraryProject.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMappint("/admin")
+
+public class AdminController {
+    
+}
---
+++ src/main/java/com/Team3/LibraryProject/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Team3.LibraryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+
+}
